d5a575fc25dd67d50fb038eba9eca470886a2aae
Fernando Biaus <biaus.fh@gmail.com>
2021-05-28 11:19:23 -0300

    X

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './App.css'
+import axios from 'axios'
 
 const App = () => {
+  const [mail, setMail] = useState({destinatario: '', asunto: '', cuerpo: ''})
+
+  const leerInput = e => {
+    const campo = e.target.name
+    const valor = e.target.value
+    setMail({
+      ...mail,
+      [campo]: valor
+    })
+  }
+
+  const enviarMail = e => {
+    e.preventDefault()
+    axios.post('http://localhost:4000/mail', mail)
+   .then(() => {
+      alert("Mensaje enviado!")
+      setMail({destinatario: '', asunto: '', cuerpo: ''})
+   })
+  }
+
   return (
     <div>
-      <h1>App</h1>
+      <form onSubmit={enviarMail}>
+        <input type="text" name="destinatario" placeholder="Dirección del destinatario" 
+        value={mail.destinatario} onChange={leerInput}/>
+        <input type="text" name="asunto" placeholder="Asunto" 
+        value={mail.asunto} onChange={leerInput}/>
+        <textarea name="cuerpo" placeholder="Cuerpo del mensaje"
+        value={mail.cuerpo} onChange={leerInput}></textarea>
+        <input type="submit" value="Enviar Mail" />
+      </form>
     </div>
   )
 }
--- server.js
+++ server.js
@@ -1,10 +1,50 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const nodemailer = require('nodemailer')
 
 const app = express()
 
+let transport = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+        user: 'pruebitamindhub@gmail.com',
+        pass: 'mindhub21'
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+})
+
+const amigos = ["Carlos", "Roberto", "Maria"]
 app.use(express.json())
 app.use(cors())
+
+app.post('/mail', (req, res) => {
+    const {destinatario, asunto, cuerpo} = req.body
+
+    // mandar el mail
+    let mailOptions = {
+        from: 'Compras Efectuadas  <nocontestar@donotreply.com>',
+        to: destinatario,
+        subject: 'Gracias por tu compra!',
+        html: `<h1>Hola</h1>
+        <script>
+        alert("Hola")
+        </script>`
+    }
+    transport.sendMail(mailOptions, (err) => {
+        if (err) console.log(err)
+        res.json({success: true})
+    })
+})
+
+
+function funcionAmigos() {
+    var html = ``
+    amigos.map(amigo => html += `<h1>${amigo}</h1>`)
+    return html
+}
 
 app.listen(4000, () => console.log("App listening on port 4000"))
